f01a78763aa46c2adc5fbb4b232449f882882a17
deeksha garg <gargdeeksha1405@gmail.com>
2026-09-19 23:53:08 +0530

    Time: 5 ms (95.79%) | Memory: 56.1 MB (34.86%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.java
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.java
@@ -1,24 +1,54 @@
-class Solution {
-    public int maximumSum(int[] arr) {
-        int noDelete = arr[0];
-        int ans = arr[0];
-        int oneDelete = 0;
+// class Solution {
+//     public int maximumSum(int[] arr) {
+//         int noDelete = arr[0];
+//         int ans = arr[0];
+//         int oneDelete = 0;
 
-        for(int i =1 ; i <arr.length; i++){
-            int oldNoDelete = noDelete;
+//         for(int i =1 ; i <arr.length; i++){
+//             int oldNoDelete = noDelete;
 
-            noDelete = Math.max(arr[i], noDelete + arr[i]);
+//             noDelete = Math.max(arr[i], noDelete + arr[i]);
 
-            oneDelete = Math.max(oldNoDelete, oneDelete + arr[i]);
+//             oneDelete = Math.max(oldNoDelete, oneDelete + arr[i]);
 
-            ans = Math.max(ans,  Math.max(noDelete, oneDelete));
-        }
-        return ans;
-    }
-}
+//             ans = Math.max(ans,  Math.max(noDelete, oneDelete));
+//         }
+//         return ans;
+//     }
+// }
 
 // noDelete
 // = max(current, previous + current)
 
 // oneDelete
 // = max(oldNoDelete, previousOneDelete + current)
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int maximumSum(int[] arr) {
+       int noDelete = arr[0];
+       int oneDelete = 0;
+       int ans = arr[0];
+
+       for(int i =1; i<arr.length; i++){
+            int oldSum = noDelete;
+
+            noDelete = Math.max(arr[i], noDelete + arr[i]);
+
+            oneDelete = Math.max(oldSum, oneDelete+arr[i]);
+
+            ans = Math.max(ans, Math.max(noDelete, oneDelete));
+         }
+         return ans;
+    }
+}
