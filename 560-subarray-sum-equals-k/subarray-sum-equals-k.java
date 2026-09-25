dc599eaf1892716771f16404d3acdb079b117d4b
deeksha garg <gargdeeksha1405@gmail.com>
2026-09-25 07:24:10 +0530

    Time: 23 ms (92.77%) | Memory: 49.1 MB (22.73%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0,1);
+        int prefix = 0;
+        int count =0;
+
+        for(int num : nums){
+            prefix += num;
+
+            int required = prefix - k;
+
+            if(map.containsKey(required)){
+                count += map.get(required); //value(freq) ko dega not key
+            }
+
+            map.put(prefix, map.getOrDefault(prefix, 0)+1); //phlele prefix jayega then usme frequency add hogi
+        }
+
+        return count;
+    }
+}
